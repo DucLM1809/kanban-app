785bfcc57e3083cec4abebcf58d4767d2d9ebef4
minh duc <minhductxqt@gmail.com>
2022-07-28 23:15:27 +0700

    add drag container

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import {
   Box,
   Drawer,
   IconButton,
   List,
   ListItem,
+  ListItemButton,
   Typography,
 } from "@mui/material";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
@@ -14,7 +15,7 @@ import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
 import assets from "../../assets/index";
 import boardApi from "../../api/boardApi";
 import { setBoards } from "../../redux/features/boardSlice";
-import { DragDropContext, Draggable, Droppable  } from "react-beautiful-dnd"
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 const Sidebar = () => {
   const user = useSelector((state) => state.user.value);
@@ -31,33 +32,29 @@ const Sidebar = () => {
       try {
         const res = await boardApi.getAll();
         dispatch(setBoards(res));
-        if (res.length > 0 && boardId === undefined) {
-          navigate(`/boards/${res[0].id}`);
-        }
+       
       } catch (err) {
         alert(err);
       }
     };
     getBoards();
-  }, []);
+  }, [dispatch]);
 
   useEffect(() => {
-    updateActive(boards);
-  }, [boards]);
-
-  const updateActive = (listBoards) => {
-    const activeItem = listBoards.findIndex((e) => e.id === boardId);
+   
+    const activeItem = boards.findIndex((e) => e.id === boardId);
+    if (boards.length > 0 && boardId === undefined) {
+      navigate(`/boards/${boards[0].id}`);
+    }
     setActiveIndex(activeItem);
-  };
+  }, [boards, boardId, navigate]);
 
   const logout = () => {
     localStorage.removeItem("token");
     navigate("/login");
   };
 
-  const onDragEnd = () => {
-    
-  }
+  const onDragEnd = () => {};
 
   return (
     <Drawer
@@ -129,7 +126,47 @@ const Sidebar = () => {
           </Box>
         </ListItem>
         <DragDropContext onDragEnd={onDragEnd}>
-
+          <Droppable
+            key={"list-board-droppable"}
+            droppableId={"list-board-droppable"}
+          >
+            {(provided) => (
+              <div ref={provided.innerRef} {...provided.droppableProps}>
+                {boards.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <ListItemButton
+                        ref={provided.innerRef}
+                        {...provided.dragHandleProps}
+                        {...provided.draggableProps}
+                        selected={index === activeIndex}
+                        component={Link}
+                        to={`/boards/${item.id}`}
+                        sx={{
+                          pl: "20px",
+                          cursor: snapshot.isDragging
+                            ? "grab"
+                            : "pointer!important",
+                        }}
+                      >
+                        <Typography
+                          variant="body2"
+                          fontWeight="700"
+                          sx={{
+                            whiteSpace: "nowrap",
+                            overflow: "hidden",
+                            textOverflow: "ellipsis",
+                          }}
+                        >
+                          {item.icon} {item.title}
+                        </Typography>
+                      </ListItemButton>
+                    )}
+                  </Draggable>
+                ))}
+              </div>
+            )}
+          </Droppable>
         </DragDropContext>
       </List>
     </Drawer>
