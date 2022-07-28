9ef85be42a6efa6a3e999d6b84b932ebaeac1bf0
minh duc <minhductxqt@gmail.com>
2022-07-28 15:46:25 +0700

    save user

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Box } from "@mui/material";
 import { Outlet, useNavigate } from "react-router-dom";
 import { Loading, Sidebar } from "../common";
+import { useDispatch } from "react-redux";
 import authUtils from "../../utils/authUtils";
+import { setUser } from "../../redux/features/userSlice";
 
 const AppLayout = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -15,6 +18,7 @@ const AppLayout = () => {
         navigate("/login");
       } else {
         // save user
+        dispatch(setUser(user));
         setLoading(false);
       }
     };
@@ -30,11 +34,13 @@ const AppLayout = () => {
       }}
     >
       <Sidebar />
-      <Box sx={{
-        flexGrow: 1,
-        p: 1,
-        width: "max-content"
-      }}>
+      <Box
+        sx={{
+          flexGrow: 1,
+          p: 1,
+          width: "max-content",
+        }}
+      >
         <Outlet />
       </Box>
     </Box>
